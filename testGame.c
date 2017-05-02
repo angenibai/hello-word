1b6455c38a1bbadac625b583ab20fa5fa22d973c
geniebai <angenibai@gmail.com>
2017-05-02 22:47:18 +1000

    Got work so far compiling

--- testGame.c
+++ testGame.c
@@ -18,44 +18,65 @@
 	                STUDENT_MMONEY, STUDENT_MTV, STUDENT_BQN, STUDENT_BPS}
 #define DEFAULT_DICE {9,10,8,12,6,5,3,11,3,11,4,6,4,7,9,2,8,10,5}
 
+void testNewGame (void); //Angeni
+//void testDisposeGame (void); 
+void testMakeAction (void); //Angeni
+//void testThrowDice (void); //Christina
+
+//void testGetDiscipline (void); //Angeni
+//void testGetDiceValue (void); //Christina - NOT DONE
+void testGetMostARCs (void); //Angeni
+void testGetMostPublications (void); //Christina
+void testGetTurnNumber (void); //Angeni
+void testGetWhoseTurn (void); //Christina - DONE
+void testGetCampus (void); //Angeni
+//void testGetARC (void); //Christina
+/*
+void testIsLegalAction (void); //Christina
+
+void testgetKPIPoints (void); //Angeni
+void testGetARCs (void); //Christina
+void testGetGO8s (void); //Angeni
+void testGetCampuses (void); //Angeni
+void testGetIPs (void); //Christina
+void testGetPublications (void); //Christina
+void testGetStudents (void); //Angeni
+void testGetExchangeRate (void); //Christina
+*/
+
 int main (int argc, char *argv[]) {
 
-
-	// create the default game called g
-	
-	
-	int disciplines[] = DEFAULT_DISCIPLINES;
-	int dice[] = DEFAULT_DICE;
-	Game g = newGame (disciplines, dice);
-
 	printf ("BEGINNING TESTING\n");
-	// none of these tests are written yet
-	testNewGame();
-	testDisposeGame();
-	testMakeAction();
-	testThrowDice();
+	// some of these tests aren't written yet
+	testNewGame(); //Angeni
+	//testDisposeGame(); 
+	testMakeAction(); //Angeni - started
+	//testThrowDice(); //Christina
 
-	testGetDiscipline();
-	testGetDiceValue();
-	testGetMostARCs();
-	testGetMostPublications();
-	testGetTurnNumber();
-	testGetWhoseTurn();
-	testGetCampus();
-	testGetARC();
+	//testGetDiscipline(); //Angeni
+	//testGetDiceValue(); //Christina - NOT DONE
+	testGetMostARCs(); //Angeni - DONE
+	testGetMostPublications(); //Christina
+	testGetTurnNumber(); //Angeni - DONE
+	testGetWhoseTurn(); //Christina - DONE
+	testGetCampus(); //Angeni - done
+	//testGetARC(); //Christina
+	/*
+	testIsLegalAction(); //Christina
 
-	testIsLegalAction();
-
-	testgetKPIPoints();
-	testGetARCs();
-	testGetGO8s();
-	testGetCampuses();
-	testGetIPs();
-	testGetPublications();
-	testGetStudents();
-	testGetExchangeRate();
+	testgetKPIPoints(); //Angeni
+	testGetARCs(); //Christina
+	testGetGO8s(); //Angeni
+	testGetCampuses(); //Angeni
+	testGetIPs(); //Christina
+	testGetPublications(); //Christina
+	testGetStudents(); //Angeni
+	testGetExchangeRate(); //Christina
+	*/
 
 	printf ("All tests passed, you are Awesome!\n");
+
+	return EXIT_SUCCESS;
 }
 
 // Angeni
@@ -70,7 +91,7 @@ void testNewGame (void) {
 	// no turns yet
 	assert (getTurnNumber (g) == -1);
 	// no one should have most ARCs
-	assert (getMostARCS (g) == NO_ONE);
+	assert (getMostARCs (g) == NO_ONE);
 	// same with most publications
 	assert (getMostPublications (g) == NO_ONE);
 	// no one's turn
@@ -79,9 +100,13 @@ void testNewGame (void) {
 	assert (getCampus (g, "") == UNI_A);
 	assert (getCampus (g, "L") == VACANT_VERTEX);
 
+	disposeGame (g);
+
 	printf (" ...newGame passes!\n");
 }
 
+
+// Angeni
 void testMakeAction (void) {
 
 	printf (" Testing makeAction\n");
@@ -94,18 +119,69 @@ void testMakeAction (void) {
 	throwDice (g, 6); // UNI_A gets MJ. UNI_A's turn
 	action a;
 	a.actionCode = OBTAIN_ARC;
-	a.destination = "L";
+	strcpy (a.destination, "L");
 	makeAction (g, a); // UNI_A builds ARC
-	assert (getArc (g, "L") == ARC_A); // checks that the action has happened
+	assert (getARC (g, "L") == ARC_A); // checks that the action has happened
 	// I should continue this
+
+	disposeGame (g);
 
 	printf (" ...makeAction passes!\n");
 
 }
+// Angeni
+void testGetDiscipline (void) {
 
+	printf ( "Testing getDiscipline\n");
+
+	// create game
+	int disciplines[] = DEFAULT_DISCIPLINES;
+	int dice[] = DEFAULT_DICE;
+	Game g = newGame (disciplines, dice);
+
+
+
+	printf (" ... getDiscipline passes!\n");
+}
+
+/*
+//Christina
+void testGetDiceValue (void) {
+	printf ("Testing getDiceValue\n");
+
+	int disciplines[] = DEFAULT_DISCIPLINES;
+	int dice[] = DEFAULT_DICE;
+	Game g = newGame (disciplines, dice);
+
+	//after throwing a 6 the value is then a 6
+	throwDice (g, 6);
+	assert (getDiceValue (g) == 6);
+
+	//after throwing a 2 the value is then a 2
+	throwDice (g, 2);
+	assert (getDiceValue (g) == 2);
+
+	//after throwing a 11 the value is then a 11
+	throwDice (g, 11);
+	assert (getDiceValue (g) == 11);
+
+	//after throwing a 9 the value is then a 9
+	throwDice (g, 9);
+	assert (getDiceValue (g) == 9);
+
+	//after throwing a 3 the value is then a 3
+	throwDice (g, 3);
+	assert (getDiceValue (g) == 3);
+
+   	disposeGame (g);
+	printf ("...getDiceValue passes!\n");
+}
+*/
+
+// Angeni
 void testGetMostARCs (void) {
 
-	printf (" Testing getMostARCS\n");
+	printf (" Testing getMostARCs\n");
 
 	// create game
 	int disciplines[] = DEFAULT_DISCIPLINES;
@@ -113,18 +189,18 @@ void testGetMostARCs (void) {
 	Game g = newGame (disciplines, dice);
 
 	// at this point no one has any
-	assert (getMostARCS (g) == NO_ONE);
+	assert (getMostARCs (g) == NO_ONE);
 	// start the turns
 	throwDice (g, 11); // UNI_A gets MTV
 	action a;
 	a.actionCode = OBTAIN_ARC;
-	a.destination = "L";
+	strcpy (a.destination, "L");
 	makeAction (g, a); // UNI_A builds ARC
-	assert (getMostARCS (g) == UNI_A);
+	assert (getMostARCs (g) == UNI_A);
 
 	throwDice (g, 9); // UNI_B gets B?
 	a.actionCode = OBTAIN_ARC;
-	a.destination = "RRLRLL";
+	strcpy (a.destination, "RRLRLL");
 	makeAction (g, a); // UNI_B builds ARC
 
 	throwDice (g, 5); // UNI_B gets BPS
@@ -136,13 +212,76 @@ void testGetMostARCs (void) {
 
 	throwDice (g, 5); // UNI_B gets BPS
 	a.actionCode = OBTAIN_ARC;
-	a.destination = "RRLRLLL";
+	strcpy (a.destination, "RRLRLLL");
 	makeAction (g, a); // UNI_B builds ARC
-	assert (getMostARCS (g) == UNI_B);
+	assert (getMostARCs (g) == UNI_B);
+
+	disposeGame (g);
 
 	printf (" ... makeAction passed\n");
 }
 
+//Christina
+void testGetMostPublications (void) {
+
+   	printf(" Testing getMostPublications");
+   	
+   	//create game
+   	int disciplines[] = DEFAULT_DISCIPLINES;
+   	int dice[] = DEFAULT_DICE;
+   	Game g = newGame (disciplines, dice);
+   
+   	//no turns as of now
+   	assert (getMostPublications (g) == 0);
+
+   	//TO DO
+
+
+   	disposeGame (g);
+	printf (" ...getMostPublications passes!\n");
+
+}
+
+//Christina
+void testGetWhoseTurn (void) {
+
+	printf ("Testing getWhoseTurn\n");
+
+	// create game
+	int disciplines[] = DEFAULT_DISCIPLINES;
+	int dice[] = DEFAULT_DICE;
+	Game g = newGame (disciplines, dice);
+
+	//1st turn should be UNI_A's
+	throwDice (g, 8);
+	assert(getWhoseTurn(g) == UNI_A);
+
+	//throwing dice moves onto next turn
+	//next turn should be UNI_B's
+	throwDice (g, 3);
+	assert(getWhoseTurn(g) == UNI_B);
+
+	//repeat for UNI_C
+	throwDice (g, 11);
+	assert(getWhoseTurn(g) == UNI_C);
+
+	//repeat for one more cycle
+	throwDice (g, 4);
+	assert(getWhoseTurn(g) == UNI_A);
+	
+	throwDice (g, 9);
+	assert(getWhoseTurn(g) == UNI_B);
+	
+	throwDice (g, 3);
+	assert(getWhoseTurn(g) == UNI_C);
+
+	disposeGame (g);
+	printf ("...getWhoseTurn passes!\n");
+
+}
+
+
+// Angeni
 void testGetTurnNumber (void) {
 
 	printf (" Testing getTurnNumber\n");
@@ -162,16 +301,20 @@ void testGetTurnNumber (void) {
 	throwDice (g, 9);
 	assert (getTurnNumber (g) == 3);
 
+	disposeGame (g);
+
 	printf (" .... getTurnNumber passed\n");
 }
 
+// Angeni
 void testGetCampus (void) {
+
+	printf (" Testing getCampus\n");
 
 	int disciplines[] = DEFAULT_DISCIPLINES;
 	int dice[] = DEFAULT_DICE;
 	Game g = newGame (disciplines, dice);
 
-	printf (" Testing getCampus\n");
 	//
 	assert (getCampus(g, "") == UNI_A);
 	assert (getCampus(g, "LRRLRLRLRLR") == UNI_A);
@@ -180,5 +323,12 @@ void testGetCampus (void) {
 	assert (getCampus(g, "LRLRL") == UNI_C);
 	assert (getCampus(g, "RRLRLLRLRL") == UNI_C);
 
-	throwDice (g, 6);
+	//throwDice (g, 6);
+	// cbbs to do more turns
+
+	disposeGame (g);
+
+	printf (" .... getCampus passed\n");
 }
+
+// Angeni
